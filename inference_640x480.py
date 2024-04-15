0205761e14d64ab281e8c6d28346aacf515c9a5b
annaayeon <ayeon52@kookmin.ac.kr>
2024-04-15 19:14:42 +0900

    Fix : PIL Error

--- inference_640x480.py
+++ inference_640x480.py
@@ -3,13 +3,14 @@ from __future__ import print_function
 import os
 import cv2
 import imageio
-import PIL.Image
+from PIL import Image
 import PIL.ImageOps as ImageOps
 import numpy as np
 import tensorflow as tf
 from button_recognition import ButtonRecognizer
 
-DRAW = False
+DRAW = True
+# DRAW = False
 
 def get_image_name_list(target_path):
   assert os.path.exists(target_path)
@@ -26,17 +27,18 @@ def warm_up(model):
   model.predict(image)
 
 if __name__ == '__main__':
-    data_dir = './test_panels'
-    data_list = get_image_name_list(data_dir)
-    recognizer = ButtonRecognizer(use_optimized=True)
-    warm_up(recognizer)
-    overall_time = 0
-    for data in data_list:
-      img_path = os.path.join(data_dir, data+'.jpg')
-      image = PIL.Image.open(tf.gfile.GFile(img_path))
+  data_dir = './test_panels'
+  data_list = get_image_name_list(data_dir)
+  recognizer = ButtonRecognizer(use_optimized=True)
+  warm_up(recognizer)
+  overall_time = 0
+  for data in data_list:
+    img_path = os.path.join(data_dir, data+'.jpg')
+    with open(img_path, 'rb') as f:
+      image = Image.open(f)
       # resize to 640x480 with ratio kept
-      img_thumbnail = image.thumbnail((640, 480), PIL.Image.ANTIALIAS)
-      delta_w, delta_h= 640 - image.size[0], 480 - image.size[1]
+      image = image.resize((640, 480))
+      delta_w, delta_h = 640 - image.size[0], 480 - image.size[1]
       padding = (delta_w // 2, delta_h // 2, delta_w - (delta_w // 2), delta_h - (delta_h // 2))
       new_im = ImageOps.expand(image, padding)
       img_np = np.copy(np.asarray(new_im))
@@ -44,13 +46,15 @@ if __name__ == '__main__':
       t0 = cv2.getTickCount()
       recognizer.predict(img_np, draw=DRAW)
       t1 = cv2.getTickCount()
-      time = (t1-t0)/cv2.getTickFrequency()
+      time = (t1 - t0) / cv2.getTickFrequency()
       overall_time += time
-      print('Time elapsed: {}'.format(time))
+      print('Time elapsed: {}'.format(time)) 
       if DRAW:
-        image = PIL.Image.fromarray(img_np)
-        image.show()
+          image = Image.fromarray(img_np)
+          image.show()
+
+
 
     average_time = overall_time / len(data_list)
     print('Average_used: {}'.format(average_time))
-    recognizer.clear_session()
+    # recognizer.clear_session()
