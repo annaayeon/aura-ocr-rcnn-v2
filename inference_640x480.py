e18637e7acc7d7c34f88ba6e2e6e97349b2cc4c4
annaayeon <ayeon52@kookmin.ac.kr>
2024-04-30 11:12:06 +0900

    Feat : box tf publisher

--- inference_640x480.py
+++ inference_640x480.py
@@ -8,14 +8,40 @@ import numpy as np
 import tensorflow as tf
 from button_recognition import ButtonRecognizer
 import pyrealsense2 as rs 
+import rospy
+import tf2_ros
+from geometry_msgs.msg import TransformStamped
 
 DRAW = True
 # DRAW = False
 
-def warm_up(model):
-  assert isinstance(model, ButtonRecognizer)
-  image = imageio.imread('./test_panels/1.jpg')
-  model.predict(image)
+class BoxTFPublisher:
+    def __init__(self):
+        rospy.init_node('bounding_box_tf_publisher')
+        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.frame_id = 'camera_frame' 
+
+    def publish_transforms(self, boxes_xyd):
+        # x 값에 따라 오름차순 정렬
+        sorted_boxes = sorted(boxes_xyd, key=lambda box: box[0])
+        
+        for i, box in enumerate(sorted_boxes):
+            center_x, center_y, depth = box
+            self.send_transform(center_x, center_y, depth, i)
+
+    def send_transform(self, x, y, d, box_id):
+        t = TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = self.frame_id
+        t.child_frame_id = f"box_{box_id+1}"
+        t.transform.translation.x = x
+        t.transform.translation.y = y
+        t.transform.translation.z = d
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+        self.broadcaster.sendTransform(t)
 
 if __name__ == '__main__':
   pipeline = rs.pipeline()
@@ -25,9 +51,11 @@ if __name__ == '__main__':
   pipeline.start(config)
 
   recognizer = ButtonRecognizer(use_optimized=True)
-  warm_up(recognizer)
+  box_publisher = BoxTFPublisher()
+  rate = rospy.Rate(10)
+
   try:
-    while True:
+    while not rospy.is_shutdown():
       frames = pipeline.wait_for_frames()
       depth_frame = frames.get_depth_frame()
       color_frame = frames.get_color_frame()
@@ -36,15 +64,20 @@ if __name__ == '__main__':
       color_image = np.asanyarray(color_frame.get_data())
       # perform button recognition
       t0 = cv2.getTickCount()
-      recognizer.predict(color_image, depth_frame, draw=DRAW)
+      recognition_list, boxes_xyd = recognizer.predict(color_image, depth_frame, draw=DRAW)
+      # recognizer.predict(color_image, depth_frame, draw=DRAW)
       t1 = cv2.getTickCount()
       time = (t1 - t0) / cv2.getTickFrequency()
       fps = 1.0 / time
       print('FPS :', fps)
+      box_publisher.publish_transforms(boxes_xyd)
       if DRAW:
           cv2.imshow('Button Recognition', color_image)
           if cv2.waitKey(1) & 0xFF == ord('q'):
               break
+  except rospy.ROSInterruptException:
+      pass
   finally:
     pipeline.stop()
+    recognizer.clear_session()
     cv2.destroyAllWindows()
