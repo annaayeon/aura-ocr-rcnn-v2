953f55b4bad696482d1eefa20fb56555de3b27df
annaayeon <ayeon52@kookmin.ac.kr>
2024-05-27 19:15:06 +0900

    Feat : Improve GPU performance by adding batch processing

--- button_recognition.py
+++ button_recognition.py
@@ -9,7 +9,7 @@ from utils import visualization_utils as vis_util
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.compat.v1.disable_eager_execution()
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # CPU 사용
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # CPU 사용
 
 charset = {'0': 0,  '1': 1,  '2': 2,  '3': 3,  '4': 4,  '5': 5,
            '6': 6,  '7': 7,  '8': 8,  '9': 9,  'A': 10, 'B': 11,
@@ -21,11 +21,12 @@ charset = {'0': 0,  '1': 1,  '2': 2,  '3': 3,  '4': 4,  '5': 5,
            '*': 42, '%': 43, '?': 44, '!': 45, '+': 46} # <nul> = +
 
 class ButtonRecognizer:
-  def __init__(self, rcnn_path= None, ocr_path=None, precision='FP16', use_optimized=False):
+  def __init__(self, rcnn_path=None, ocr_path=None, precision='FP16', use_optimized=False, batch_size=1):
     self.ocr_graph_path = ocr_path
     self.rcnn_graph_path = rcnn_path
-    self.precision=precision  #'INT8, FP16, FP32'
+    self.precision = precision  #'INT8, FP16, FP32'
     self.use_optimized = use_optimized
+    self.batch_size = batch_size
     self.session = None
 
     self.ocr_input = None
@@ -96,9 +97,9 @@ class ButtonRecognizer:
       rcnn_scores = ocr_rcnn_graph.get_tensor_by_name('detection/detection_scores:0')
       rcnn_number = ocr_rcnn_graph.get_tensor_by_name('detection/num_detections:0')
 
-      # crop and resize valida boxes (only valid when rcnn input has an known shape)
+      # crop and resize valid boxes (only valid when rcnn input has an known shape)
       rcnn_number = tf.cast(rcnn_number, tf.int32)
-      valid_boxes = tf.slice(rcnn_boxes, [0, 0, 0], [1, rcnn_number[0], 4])
+      valid_boxes = tf.slice(rcnn_boxes, [0, 0, 0], [self.batch_size, rcnn_number[0], 4])
 
       ocr_boxes = native_crop_and_resize(rcnn_input, valid_boxes, self.recognition_size)
 
@@ -111,7 +112,7 @@ class ButtonRecognizer:
       self.rcnn_output = [rcnn_boxes, rcnn_scores, rcnn_number, ocr_boxes]
       self.ocr_input = ocr_input
       self.ocr_output = [ocr_chars, ocr_beliefs]
-      
+
       config = tf.compat.v1.ConfigProto()
       config.gpu_options.allow_growth = True
       self.session = tf.compat.v1.Session(graph=ocr_rcnn_graph, config=config)
@@ -130,35 +131,41 @@ class ButtonRecognizer:
     score_ave /= len(text)
     return text, score_ave
 
-  def predict(self, image_np, draw=False):
+  def predict(self, images_np, draw=False):
     # input data
-    assert image_np.shape == (480, 640, 3)
-    img_in = np.expand_dims(image_np, axis=0)
+    assert images_np.shape == (self.batch_size, 480, 640, 3)
+    img_in = images_np
 
     # output data
     recognition_list = []
 
     # perform detection and recognition
-    boxes, scores, number, ocr_boxes = self.session.run(self.rcnn_output, feed_dict={self.rcnn_input:img_in})
-    boxes, scores, number = [np.squeeze(x) for x in [boxes, scores, number]]
-
-    for i in range(number):
-        if scores[i] < 0.5: continue
-        center_x = int((boxes[i][1] + boxes[i][3]) * 0.5 * self.image_size[1])
-        center_y = int((boxes[i][0] + boxes[i][2]) * 0.5 * self.image_size[0])
-        if ocr_boxes.any():
-
-            chars, beliefs = self.session.run(self.ocr_output, feed_dict={self.ocr_input: ocr_boxes[:,i]})
-            chars, beliefs = [np.squeeze(x) for x in [chars, beliefs]]
-            text, belief = self.decode_text(chars, beliefs)
-        else:
-            text, belief = '', 0.0
-        recognition_list.append([boxes[i], scores[i], text, belief, [center_x, center_y]])
+    boxes, scores, number, ocr_boxes = self.session.run(self.rcnn_output, feed_dict={self.rcnn_input: img_in})
+    for b in range(self.batch_size):
+        batch_boxes = boxes[b]
+        batch_scores = scores[b]
+        batch_number = number[b]
+        for i in range(batch_number):
+            if batch_scores[i] < 0.5:
+                continue
+            center_x = int((batch_boxes[i][1] + batch_boxes[i][3]) * 0.5 * self.image_size[1])
+            center_y = int((batch_boxes[i][0] + batch_boxes[i][2]) * 0.5 * self.image_size[0])
+            if ocr_boxes.any():
+                chars, beliefs = self.session.run(self.ocr_output, feed_dict={self.ocr_input: ocr_boxes[:, i]})
+                chars, beliefs = [np.squeeze(x) for x in [chars, beliefs]]
+                text, belief = self.decode_text(chars, beliefs)
+            else:
+                text, belief = '', 0.0
+            recognition_list.append([batch_boxes[i], batch_scores[i], text, belief, [center_x, center_y]])
 
     if draw:
-      classes = [1]*len(boxes)
-      self.draw_detection_result(image_np, boxes, classes, scores, self.category_index)
-      self.draw_recognition_result(image_np, recognition_list)
+      for b in range(self.batch_size):
+          batch_image = images_np[b]
+          batch_boxes = boxes[b]
+          batch_scores = scores[b]
+          classes = [1] * len(batch_boxes)
+          self.draw_detection_result(batch_image, batch_boxes, classes, batch_scores, self.category_index)
+          self.draw_recognition_result(batch_image, recognition_list)
 
     return recognition_list
 
@@ -190,21 +197,21 @@ class ButtonRecognizer:
       # draw at a proper location
       x_center = (x_max-x_min) / 2.0
       y_center = (y_max-y_min) / 2.0
-      font_size = min(x_center, y_center)*1.1
-      text_center = int(x_center-0.5*font_size), int(y_center-0.5*font_size)
+      font_size = min(x_center, y_center) * 1.1
+      text_center = int(x_center - 0.5 * font_size), int(y_center - 0.5 * font_size)
       try:
           font = ImageFont.truetype('/Library/Fonts/Arial.ttf', int(font_size))  # 변경 가능한 폰트 경로
       except IOError:
           font = ImageFont.load_default() 
       img_show.text(text_center, text=item[2], font=font, fill=(255, 0, 255))
-      # img_pil.show()
       image_np[y_min: y_max, x_min: x_max] = np.array(img_pil)
 
 
 if __name__ == '__main__':
-    recognizer = ButtonRecognizer(use_optimized=False)
-    image = imageio.imread('./test_panels/1.jpg')
-    recognition_list = recognizer.predict(image, True)
-    image = Image.fromarray(image)
-    image.show()
+    recognizer = ButtonRecognizer(use_optimized=False, batch_size=4)
+    images = np.array([imageio.imread('./test_panels/{}.jpg'.format(i+1)) for i in range(4)])
+    recognition_list = recognizer.predict(images, True)
+    for img in images:
+        image = Image.fromarray(img)
+        image.show()
     recognizer.clear_session()
